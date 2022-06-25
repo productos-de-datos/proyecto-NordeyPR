d22ab8cfcaf4320340d3e21c8cb5608a9e51162f
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-25 10:46:01 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -5,7 +5,9 @@ Módulo de ingestión de datos.
 """
 
 
+
 def ingest_data():
+    
     """Ingeste los datos externos a la capa landing del data lake.
 
     Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
@@ -13,6 +15,9 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
+
+   
+    
     raise NotImplementedError("Implementar esta función")
 
 
