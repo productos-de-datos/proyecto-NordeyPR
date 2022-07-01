ad8c9a5a832c378241311dd85c250d0c211caa77
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-07-01 17:12:57 -0500

    Update make_forecasts.py

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -74,7 +74,7 @@ def make_forecasts():
 
         df_model_RF = pd.concat([df_model_RF, df_series], axis=1)
 
-        data = data[['fecha']]
+        data = data[['Fecha']]
         data.reset_index(inplace=True)
 
         df_model_RF = pd.merge(df_model_RF, data, on='index', how='left')
