938737eb7be783aa02319408501b3007be4ff546
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-07-01 16:59:39 -0500

    Update make_forecasts.py

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -1,13 +1,3 @@
-import pickle
-import os
-import pandas as pd
-import subprocess
-
-subprocess.call(["pip", "install", "skforecast"])
-
-from skforecast.model_selection import backtesting_forecaster
-
-
 def make_forecasts():
     """Construya los pronosticos con el modelo entrenado final.
     Cree el archivo data_lake/business/forecasts/precios-diarios.csv. Este
@@ -15,52 +5,102 @@ def make_forecasts():
     * La fecha.
     * El precio promedio real de la electricidad.
     * El pronóstico del precio promedio real.
-    >>> make_forecasts()
     """
+    from train_daily_model import train_daily_model, load_data, data_preparation, make_train_test_split
 
-    modelo = cargar_modelo()
+    def load_pkl():
+        import pickle
+        with open("src/models/precios-diarios.pickle", "rb") as file:
+            model_RF = pickle.load(file)
+        return model_RF
 
-    datos = pd.read_csv("data_lake/business/features/precios_diarios.csv")
-    datos["Fecha"] = pd.to_datetime(datos["Fecha"], format="%Y-%m-%d")
-    datos = datos.set_index("Fecha")
-    datos = datos.asfreq("D")
-    datos = datos.sort_index()
+    def score(x_train, y_train):
+        import numpy as np
 
-    datos_backtest = 365 * 20
-    datos_train = datos[:-datos_backtest]
-    datos_test = datos[-datos_backtest:]
+        # Se agrega distintas estimaciones
+        estimators = np.arange(10, 200, 10)
+        scores = []
+        for n in estimators:
+            model_RF.set_params(n_estimators=n)
+            model_RF.fit(x_train, y_train)
+            scores.append(model_RF.score(x_test, y_test))
 
-    modelo.fit(y=datos_train["Precio"])
+        return scores
 
-    metrica, predicciones = backtesting_forecaster(
-        forecaster=modelo,
-        y=datos.Precio,
-        initial_train_size=len(datos_train),
-        fixed_train_size=False,
-        steps=1,
-        metric="mean_absolute_error",
-        refit=False,
-        verbose=True,
-    )
+    def best_score(scores):
 
-    predicciones.join(datos_test["Precio"]).to_csv(
-        "data_lake/business/forecasts/precios-diarios.csv", index=True
-    )
+        import pandas as pd
+        estimador_n = pd.DataFrame(scores)
+        estimador_n.reset_index(inplace=True)
+        estimador_n = estimador_n.rename(columns={0: 'scores'})
+        estimador_n = estimador_n[estimador_n['scores']
+                                  == estimador_n['scores'].max()]
+        estimador_n['index'] = (estimador_n['index']+1)*10
+        estimador_n = estimador_n['index'].iloc[0]
+
+        return estimador_n
+
+    def trein_model_with_best_estimator(estimador_n):
+
+        from sklearn.ensemble import RandomForestRegressor
+
+        # Entrenamiento del moodelo con los estimadores sugeridos
+        model_RF = RandomForestRegressor(
+            n_estimators=estimador_n, random_state=12345)
+        model_RF.fit(x_train, y_train)
+        return model_RF
+
+    def prediction_test_model(model_RF):
+        # Prediccion de x_test
+        y_pred_RF_testeo = model_RF.predict(x_test)
+        return y_pred_RF_testeo
+
+    def forecasts(y_pred_RF_testeo, y_test, data):
+        import pandas as pd
+        import numpy as np
+
+        df_model_RF = pd.DataFrame(y_test).reset_index(drop=True)
+        df_model_RF['pronostico'] = y_pred_RF_testeo
+
+        np.random.seed(0)
+
+        # idenfificar indices de y_test
+        # ------------------------------------------
+        df_series = pd.Series(y_test)
+        df_series = df_series.to_frame()
+        df_series.reset_index(inplace=True)
+        df_series = df_series[['index']]
+        # ------------------------------------------
+
+        df_model_RF = pd.concat([df_model_RF, df_series], axis=1)
+
+        data = data[['fecha']]
+        data.reset_index(inplace=True)
+
+        df_model_RF = pd.merge(df_model_RF, data, on='index', how='left')
+        df_model_RF = df_model_RF[['Fecha', 'Precio', 'pronostico']]
+        return df_model_RF
+
+    def save_forecasts(df_model_RF):
+        df_model_RF.to_csv(
+            'data_lake/business/forecasts/precios-diarios.csv', index=None)
+
+    # ejecución
+    data = load_data()
+    x, y = data_preparation(data)
+    x_train, x_test, y_train, y_test = make_train_test_split(x, y)
+    model_RF = load_pkl()
+    scores = score(x_train, y_train)
+    estimador_n = best_score(scores)
+    model_RF = trein_model_with_best_estimator(estimador_n)
+    y_pred_RF_testeo = prediction_test_model(model_RF)
+    df_model_RF = forecasts(y_pred_RF_testeo, y_test, data)
+    save_forecasts(df_model_RF)
 
 
-def cargar_modelo():
-    """Cargue el modelo entrenado final.
-    Retorne el modelo entrenado final.
-    """
-    with open("src/models/precios-diarios.pkl", "rb") as file:
-        modelo = pickle.load(file)
-
-    return modelo
-
-
+    #raise NotImplementedError("Implementar esta función")
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-    
-    
+    make_forecasts()
